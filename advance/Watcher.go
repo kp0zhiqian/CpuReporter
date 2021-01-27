209b6455f4f569334903121efddf74b85bcc38f4
zhguan <zhiqian0923@gmail.com>
2021-01-27 22:32:36 +0800

    change collection way

--- advance/Watcher.go
+++ advance/Watcher.go
@@ -22,30 +22,21 @@ type Timeline struct {
 	Data []CpusTask
 }
 
-func getTask() []CpusTask {
-	var Results []CpusTask
+func getTask(cpuNum string) []string {
+	var Results []string
 	file, err := os.Open("sched_debug")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-
-	var cpu_i int64
 	
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.HasPrefix(line, "cpu#") {
-			var SingleCpu CpusTask
-			cpuTag := strings.Split(strings.Split(line, ",")[0], "#")[1]
-			cpu_i, _ = strconv.ParseInt(cpuTag, 10, 64)
-			percentPerCpu, _ := cpu.Percent(time.Second*1, true)
-			SingleCpu.Usage = float64(percentPerCpu[cpu_i])
-			fmt.Println(SingleCpu.Usage)
-			SingleCpu.Cpu = cpuTag
-			Results = append(Results, SingleCpu)
+		if strings.HasPrefix(line, "cpu#"+cpuNum) {
+			Results = append(Results, line)
 		} else if strings.HasPrefix(line, " S") || strings.HasPrefix(line, " I") || strings.HasPrefix(line, ">R") {
-			Results[cpu_i].Task = append(Results[cpu_i].Task, line)
+			Results = append(Results, line)
 		}
 	}
 	
@@ -66,10 +57,13 @@ func main() {
 	defer f.Close()
 	enc := json.NewEncoder(f)
 	for {
-		cpuTaskNow := getTask()
 		result.Second = second
-		result.Data = cpuTaskNow
-		time.Sleep(1 * time.Second)
+		percentPerCpu, _ := cpu.Percent(time.Second*1, true)
+		for i, usg := percentPerCpu {
+			result.Data[i].Usage = float64(usg)
+			result.Data[i].CPU = i
+			result.Data[i].Task = getTask()
+		}
 		enc.Encode(result)
 		second += 1
 	}
