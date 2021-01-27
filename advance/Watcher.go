7d2fd86edf1305a01afa166df0a8b2a8650e578f
zhguan <zhiqian0923@gmail.com>
2021-01-27 22:51:19 +0800

    fix index

--- advance/Watcher.go
+++ advance/Watcher.go
@@ -61,7 +61,7 @@ func main() {
 		for i, usg := range percentPerCpu {
 			var everySecond CpusTask
 			everySecond.Usage = float64(usg)
-			everySecond.Cpu = fmt.Printf("%d", i)
+			everySecond.Cpu, _ = fmt.Printf("%d", i)
 			everySecond.Task = getTask(string(i))
 			result.Data = append(result.Data, everySecond)
 		}
